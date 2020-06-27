d8c24d019d2948314d708d1da59f2d245af77a0c
Ignas Anikevicius <anikevicius@gmail.com>
2020-06-27 13:30:17 +0300

    Add an example CLI

--- tools/ci/ci-test.sh
+++ tools/ci/ci-test.sh
@@ -7,12 +7,12 @@ die() {
 }
 
 ensure() {
-    "$1"
+    "$@"
     files="$(git status --porcelain)"
     if [ -n "$files" ]; then
         echo "Detected changes to:"
         echo "$files"
-        die "Please run '$1' to cleanup the build files"
+        die "Please run '$*' to cleanup the build files"
     fi
 }
 
@@ -21,4 +21,6 @@ cd "${WORKSPACE_ROOT}"
 
 ensure tools/buildifier
 ensure tools/gazelle
+ensure tools/gazelle update-repos -from_file=src/go.mod -prune
 bazel test //:verify-all
+bazel test //src/...
