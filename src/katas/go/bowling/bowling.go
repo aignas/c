c08733a6049da0560771fa65abaca56915f82701
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-03 20:16:36 +0300

    have something working

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -1,6 +1,94 @@
 package bowling
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func newFrame(first, second int) frame {
+	return frame{
+		First:  first,
+		Second: second,
+	}
+}
+
+func newLastFrame(first, second, third int) frame {
+	return frame{
+		First:  first,
+		Second: second,
+		Third:  third,
+	}
+}
+
+type frame struct {
+	First  int
+	Second int
+	Third  int
+}
+
+func (f *frame) Sum() int {
+	return f.First + f.Second + f.Third
+}
+
+func (f *frame) isSpare() bool {
+	return f.Sum() == 10
+}
+
 // Result calculates the result of a bowling game.
-func Result(string) (int, error) {
-	return 0, nil
+func Result(input string) (int, error) {
+	frames, err := parseFrames(input)
+	if err != nil {
+		return 0, fmt.Errorf("parsing frames: %s", err)
+	}
+
+	var result int
+	for i, frame := range frames {
+		result += frame.Sum()
+
+		if i != 9 && frame.isSpare() {
+			result += frames[i+1].First
+		}
+	}
+	return result, nil
+}
+
+func parseFrames(input string) ([]frame, error) {
+	var (
+		frames = make([]frame, 10)
+		i      = 0
+		j      = 0
+	)
+
+	for _, r := range input {
+		var val int
+
+		switch r {
+		case '-':
+			// nothing
+		case '/':
+			val = 10 - frames[i].First
+		default:
+			v, err := strconv.Atoi(string(r))
+			if err != nil {
+				return nil, fmt.Errorf("bad input: %q", input)
+			}
+			val = v
+		}
+
+		if j == 0 {
+			frames[i].First = val
+			j++
+		} else if j == 1 {
+			frames[i].Second = val
+			if i == 9 {
+				j++
+			} else {
+				j = 0
+				i++
+			}
+		} else {
+			frames[i].Third = val
+		}
+	}
+	return frames, nil
 }
--- src/katas/go/bowling/bowling_test.go
+++ src/katas/go/bowling/bowling_test.go
@@ -1,6 +1,7 @@
 package bowling
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,6 +15,9 @@ func TestResult(t *testing.T) {
 		wantErr string
 	}{
 		{"--------------------", 0, ""},
+		{"5-5-5-5-5-5-5-5-5-5-", 50, ""},
+		{"9-9-9-9-9-9-9-9-9-9-", 90, ""},
+		{"5/5/5/5/5/5/5/5/5/5/5", 150, ""},
 	}
 
 	for _, tt := range tests {
@@ -28,6 +32,105 @@ func TestResult(t *testing.T) {
 				return
 			}
 			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestParseFrames(t *testing.T) {
+	wantAll := func(f frame) []frame {
+		res := make([]frame, 10)
+		for i := range res {
+			res[i] = f
+		}
+		return res
+	}
+	wantAllWithLast := func(f, last frame) []frame {
+		res := make([]frame, 10)
+		for i := range res {
+			if i == 9 {
+				res[i] = last
+			} else {
+				res[i] = f
+			}
+		}
+		return res
+	}
+
+	all := func(f string) string {
+		res := make([]string, 10)
+		for i := range res {
+			res[i] = f
+		}
+		return strings.Join(res, "")
+	}
+
+	t.Parallel()
+	tests := []struct {
+		input   string
+		want    []frame
+		wantErr string
+	}{
+		{all("--"), wantAll(newFrame(0, 0)), ""},
+		{all("5-"), wantAll(newFrame(5, 0)), ""},
+		{all("54"), wantAll(newFrame(5, 4)), ""},
+		{all("5/") + "5", wantAllWithLast(newFrame(5, 5), newLastFrame(5, 5, 5)), ""},
+		{"foo", nil, "bad input: \"foo\""},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := parseFrames(tt.input)
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr != "" {
+				assert.EqualError(t, err, tt.wantErr)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func Test_frame_sum(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		msg   string
+		frame frame
+		want  int
+	}{
+		{msg: "empty"},
+		{msg: "5-", frame: newFrame(5, 0), want: 5},
+		{msg: "5-", frame: newFrame(0, 4), want: 4},
+		{msg: "-/4", frame: newLastFrame(0, 10, 4), want: 14},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.msg, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, tt.want, tt.frame.Sum())
+		})
+	}
+}
+
+func Test_frame_isSpare(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		msg   string
+		frame frame
+		want  bool
+	}{
+		{msg: "5-", frame: newFrame(5, 0), want: false},
+		{msg: "5/", frame: newFrame(5, 5), want: true},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.msg, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, tt.want, tt.frame.isSpare())
 		})
 	}
 }
