f302214f1529645bedffe74770adf832a11149ad
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-04 10:37:19 +0300

    green: handle threes

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -1,11 +1,18 @@
 package bowling
 
-import "strings"
+import "strconv"
 
 // Score returns the bowling score.
 func Score(sheet string) (int, error) {
-	if strings.HasPrefix(sheet, "5") {
-		return 50, nil
+	var result int
+	for _, throw := range sheet {
+		switch throw {
+		case '-':
+			// nothing
+		default:
+			v, _ := strconv.Atoi(string(throw))
+			result += v
+		}
 	}
-	return 0, nil
+	return result, nil
 }
