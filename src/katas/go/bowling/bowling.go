c9b0b0b3c9282b6387ce4f185b54cf15f485c1e5
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-04 10:48:55 +0300

    refactor: use parse in Score

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -15,14 +15,9 @@ type frame struct {
 // Score returns the bowling score.
 func Score(sheet string) (int, error) {
 	var result int
-	for _, throw := range sheet {
-		switch throw {
-		case '-':
-			// nothing
-		default:
-			v, _ := strconv.Atoi(string(throw))
-			result += v
-		}
+	frames, _ := parse(sheet)
+	for _, frame := range frames {
+		result += frame.First
 	}
 	return result, nil
 }
