40d0ab75a6e8f8350b226a3c31d8d6b03bba60b8
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-04 11:10:47 +0300

    refactor: lint

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -5,6 +5,9 @@ import (
 	"strconv"
 )
 
+// _max is the number of pins that we can hit at most during one throw.
+const _max = 10
+
 func newFrame(vals ...int) frame {
 	return frame{
 		vals: vals,
@@ -19,19 +22,23 @@ func (f *frame) First() int {
 	if len(f.vals) == 0 {
 		return 0
 	}
+
 	return f.vals[0]
 }
 
 // Score returns the bowling score.
 func Score(sheet string) (int, error) {
 	var result int
+
 	frames, err := parse(sheet)
 	if err != nil {
 		return 0, err
 	}
+
 	for _, frame := range frames {
 		result += frame.First()
 	}
+
 	return result, nil
 }
 
@@ -40,20 +47,24 @@ func parse(sheet string) ([]frame, error) {
 		frames = make([]frame, 10)
 		i      int
 	)
+
 	for _, throw := range sheet {
 		var val int
+
 		switch throw {
 		case '-':
 			val = 0
 		case '/':
-			val = 10 - frames[i].First()
+			val = _max - frames[i].First()
 		default:
 			v, err := strconv.Atoi(string(throw))
 			if err != nil {
 				return nil, fmt.Errorf("bad input: %q", sheet)
 			}
+
 			val = v
 		}
+
 		frames[i].vals = append(frames[i].vals, val)
 		if i != 9 && len(frames[i].vals) == 2 {
 			i++
