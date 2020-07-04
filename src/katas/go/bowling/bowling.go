edfedc8db9f6f200fb2fc4fc567df54a8a75c77d
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-04 10:41:21 +0300

    green: parse

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -2,6 +2,8 @@ package bowling
 
 import "strconv"
 
+type frame struct{}
+
 // Score returns the bowling score.
 func Score(sheet string) (int, error) {
 	var result int
@@ -15,4 +17,8 @@ func Score(sheet string) (int, error) {
 		}
 	}
 	return result, nil
+}
+
+func parse(string) ([]frame, error) {
+	return make([]frame, 10), nil
 }
