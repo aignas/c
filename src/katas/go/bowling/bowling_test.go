b2993a1e041a5f2bc6a60ab614dbc2e8e234d652
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-04 10:32:31 +0300

    red: scaffold

--- src/katas/go/bowling/bowling_test.go
+++ src/katas/go/bowling/bowling_test.go
@@ -1,1 +1,37 @@
 package bowling
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResult(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		input   string
+		want    int
+		wantErr string
+	}{
+		{
+			input:   "--------------------",
+			want:    0,
+			wantErr: "",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := Result(tt.input)
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr != "" {
+				assert.EqualError(t, err, tt.wantErr)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
