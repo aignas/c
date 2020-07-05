53c7f3c37e7a77cae9f375d0dcde09a88066a38c
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-05 15:36:42 +0300

    start type driven exploration

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -1,6 +1,29 @@
 package bowling
 
+type frame struct{}
+
+func (*frame) Sum() int {
+	return 0
+}
+
 // Score returns bowling score
-func Score(string) (int, error) {
-	return 0, nil
+func Score(input string) (int, error) {
+	frames, err := parse(input)
+	if err != nil {
+		return 0, err
+	}
+
+	var result int
+	for _, frame := range frames {
+		result += frame.Sum()
+
+		// we need to add extras
+	}
+
+	return result, nil
+}
+
+// parse returns frames
+func parse(string) ([]frame, error) {
+	return nil, nil
 }
