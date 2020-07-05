6c886a15c17312448815946797833fc8bc1384f0
Ignas Anikevicius <anikevicius@gmail.com>
2020-07-05 16:14:45 +0300

    green

--- src/katas/go/bowling/bowling.go
+++ src/katas/go/bowling/bowling.go
@@ -2,24 +2,12 @@ package bowling
 
 type frame []int
 
-func (frame) Sum() int {
+func (frame) Score() int {
 	return 0
 }
 
-func (frame) First() int {
+func (frame) Len() int {
 	return 0
-}
-
-func (frame) Head() int {
-	return 0
-}
-
-func (frame) IsSpare() bool {
-	return false
-}
-
-func (frame) IsStrike() bool {
-	return false
 }
 
 // Score returns bowling score
@@ -29,35 +17,42 @@ func Score(input string) (int, error) {
 		return 0, err
 	}
 
-	var (
-		result        int
-		doubleFirst   bool
-		doubleNextTwo bool
-	)
+	var result int
 
 	for _, frame := range frames {
-		result += frame.Sum()
-		if doubleFirst {
-			result += frame.First()
-		}
-
-		if doubleNextTwo {
-			if frame.IsStrike() {
-				result += frame.First()
-			} else {
-				result += frame.Head()
-			}
-		}
-
-		// we need to add extras
-		doubleFirst = frame.IsSpare() || doubleNextTwo && frame.IsStrike()
-		doubleNextTwo = frame.IsStrike()
+		result += frame.Score()
 	}
 
 	return result, nil
 }
 
 // parse returns frames
-func parse(string) ([]frame, error) {
+func parse(input string) ([]frame, error) {
+	throws, err := parseThrows(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var j int
+	frames := make([]frame, 10)
+	for i := range frames {
+		if i == 9 {
+			frames[i] = newFrame(throws[j:])
+		} else {
+			frames[i] = newFrame(throws[j : j+3])
+		}
+
+		j += frames[i].Len()
+	}
+
 	return nil, nil
+}
+
+func parseThrows(input string) ([]int, error) {
+	return make([]int, len(input)), nil
+}
+
+// newFrame returns the frame and the number of throws in this frame
+func newFrame(s []int) frame {
+	return nil
 }
