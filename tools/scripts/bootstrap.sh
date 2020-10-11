981793137e86df4feb5d69159349b1d3e04c7ed9
Ignas Anikevicius <anikevicius@gmail.com>
2020-10-11 13:09:04 +0300

    update cargo-raze and bazelisk

--- tools/scripts/bootstrap.sh
+++ tools/scripts/bootstrap.sh
@@ -9,29 +9,29 @@ readonly WORKSPACE_ROOT=$(git rev-parse --show-toplevel)
 cd "${WORKSPACE_ROOT}"
 
 bazelisk() {
-    local -r url="https://github.com/bazelbuild/bazelisk/releases/download"
-    local -r version="$1"
-    local -r name="bazelisk-$2"
+  local -r url="https://github.com/bazelbuild/bazelisk/releases/download"
+  local -r version="$1"
+  local -r name="bazelisk-$2"
 
-    [[ -f ./tools/bin/bazel ]] && return
+  [[ -f ./tools/bin/bazel ]] && return
 
-    curl -L "$url/v$version/$name" \
-        --output ./tools/bin/bazel
-    chmod +x ./tools/bin/bazel
+  curl -L "$url/v$version/$name" \
+    --output ./tools/bin/bazel
+  chmod +x ./tools/bin/bazel
 }
 
 raze() {
-    local -r version="$1"
+  local -r version="$1"
 
-    [[ -f ./tools/bin/cargo-raze ]] && return
+  [[ -f ./tools/bin/cargo-raze ]] && return
 
-    cargo install cargo-raze --root=./tools --version="$1"
+  cargo install cargo-raze --root=./tools --version="$1"
 }
 
 main() {
-    mkdir -p ./tools/bin
-    bazelisk 1.5.0 linux-amd64
-    raze 0.3.8
+  mkdir -p ./tools/bin
+  bazelisk 1.7.1 linux-amd64
+  raze 0.5.0
 }
 
 main
