0425d96f4256f5a8fe7bfcfff76afadb2d9f11c9
Ignas Anikevicius <anikevicius@gmail.com>
2020-10-22 22:36:21 +0300

    shellcheck

--- src/experiments/dbinsert/run-db.sh
+++ src/experiments/dbinsert/run-db.sh
@@ -7,6 +7,6 @@ exec sudo docker run -ti --rm \
     -e MYSQL_PASSWORD=bench \
     -e MYSQL_USER=test \
     -e MYSQL_DATABASE=testbulk \
-    -v ${PWD}/init.sql:/docker-entrypoint-initdb.d/1-init.sql \
+    -v "${PWD}"/init.sql:/docker-entrypoint-initdb.d/1-init.sql \
     --net host \
-    mysql:8 
+    mysql:8
