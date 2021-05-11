7f1e80ee70b88b80052c8212e7b5f2f1c097b323
Ignas Anikevicius <anikevicius@gmail.com>
2021-05-11 19:39:03 +0300

    Upgrade to bazelisk 1.8.1 ad bazel 4.0.0 (#18)
    * upgrade bazel to v4.0.0
    
    * bootstrap: update to support OSX as well
    
    * rustfmt: support darwin

--- tools/scripts/bootstrap.sh
+++ tools/scripts/bootstrap.sh
@@ -9,20 +9,24 @@ readonly WORKSPACE_ROOT=$(git rev-parse --show-toplevel)
 cd "${WORKSPACE_ROOT}"
 
 bazelisk() {
-  local -r url="https://github.com/bazelbuild/bazelisk/releases/download"
-  local -r version="$1"
-  local -r name="bazelisk-$2"
+    local -r url="https://github.com/bazelbuild/bazelisk/releases/download"
+    local -r version="$1"
+    local -r name="bazelisk-$2"
 
-  [[ -f ./tools/bin/bazel ]] && return
+    [[ -f ./tools/bin/bazel ]] && return
 
-  curl -L "$url/v$version/$name" \
-    --output ./tools/bin/bazel
-  chmod +x ./tools/bin/bazel
+    curl -L "$url/v$version/$name" \
+        --output ./tools/bin/bazel
+    chmod +x ./tools/bin/bazel
 }
 
 main() {
-  mkdir -p ./tools/bin
-  bazelisk 1.7.1 linux-amd64
+    mkdir -p ./tools/bin
+    if [ "$(uname -s)" == "Darwin" ]; then
+        bazelisk 1.8.1 darwin-amd64
+    else
+        bazelisk 1.8.1 linux-amd64
+    fi
 }
 
 main
